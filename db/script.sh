86c22ae2e3e72cd07e7eebb935e074395f7d5e1c
neojinxd <neojinxd@gmail.com>
2023-11-09 21:43:08 -0500

    fixed docker issue with thez0ne site on initial docker setup

--- db/script.sh
+++ db/script.sh
@@ -6,5 +6,5 @@ database=$DB_NAME
 username=$DB_USER
 password=$DB_PASSWORD
 
-psql postgresql://$username:$password@$container:5432/$database -c "DELETE FROM \"Message\" WHERE \"sentAt\" < NOW() - INTERVAL '30 day'"
-# echo "This is a script, run by cron!"
+echo "Cleaning up old messages..."
+psql postgresql://$username:$password@$container:5432/$database -c "DELETE FROM \"Message\" WHERE \"sentAt\" < NOW() - INTERVAL '1 day'"
