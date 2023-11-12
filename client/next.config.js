97041bcdadceae8e02590586a27b28cde7bc8ad1
neojinxd <neojinxd@gmail.com>
2023-11-11 21:50:56 -0500

    added security headers and metadata base url

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,35 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self' 'https://avatars.githubusercontent.com/'; script-src 'self'",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "camera=(); battery=(); geolocation=(); microphone=()",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
