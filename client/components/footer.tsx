9e65afe8e2a782683e5596244f29263587c51a42
Anik Patel <neojinxd@gmail.com>
2025-10-16 16:39:35 -0400

    Update footer.tsx

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -2,13 +2,13 @@ export default function Footer() {
   return (
     <div className='flex items-center w-full text-white bg-gray-950'>
       <p className='pl-1 text-left w-1/3'>
-        &copy;Anik Patel 2023
+        &copy;Anik Patel 2025
       </p>
       <p className='mx-auto text-center w-1/3'>
-        Made with NextJS 13
+        
       </p>
       <p className='pr-1 text-right w-1/3'>
-        V1.1.0
+        V1.1.5
       </p>
     </div>
   )
